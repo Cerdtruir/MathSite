03fecda4783598f4ef72494386759d8de93407d5
Cerdtruir <ghoornabeel@gmail.com>
2022-04-08 19:55:04 +0200

    Add react router and pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
 import './index.css';
+import Nav from './Nav';
 import App from './App';
+import Home from './Home';
+import Quote from './Quote';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<App />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
