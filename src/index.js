0957f299f9e58cd58793d9d133644c9640b4fd4a
Cerdtruir <ghoornabeel@gmail.com>
2022-04-09 22:35:17 +0200

    Move app generation code to app.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
-
 import './index.css';
 
-import Nav from './components/Nav';
-import Home from './components/Home';
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
